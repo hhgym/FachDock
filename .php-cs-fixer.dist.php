725fb97899fb2ef689e3ecefe0cd4a0a914c967b
Heinrich-Hertz-Gymnasium Berlin <50577548+hhgym-berlin@users.noreply.github.com>
2026-09-08 14:17:22 +0200

    build: add quality tooling and CI checks

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__ . '/src', __DIR__ . '/migrations', __DIR__ . '/tests'])
+    ->append([__FILE__, __DIR__ . '/public/index.php']);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(false)
+    ->setRules([
+        '@PSR12' => true,
+        'declare_strict_types' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+    ])
+    ->setFinder($finder);
