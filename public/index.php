abf9b31cbae8a9a0ad7cc3fb66c535a6e6a744fa
Heinrich-Hertz-Gymnasium Berlin <50577548+hhgym-berlin@users.noreply.github.com>
2026-09-09 06:00:31 +0200

    refactor: route all requests through application

--- public/index.php
+++ public/index.php
@@ -3,9 +3,6 @@
 declare(strict_types=1);
 
 use FachDock\Application;
-use FachDock\Booking\BookingPaymentAdminEntryPoint;
-use FachDock\Http\Request;
-use FachDock\Payment\StripePaymentEntryPoint;
 
 $root = dirname(__DIR__);
 $autoload = $root . '/vendor/autoload.php';
@@ -19,17 +16,4 @@ if (!is_file($autoload)) {
 
 require $autoload;
 
-$app = Application::boot($root);
-$request = Request::fromGlobals();
-if (StripePaymentEntryPoint::handles($request)) {
-    StripePaymentEntryPoint::run($root, $request);
-
-    exit;
-}
-if (BookingPaymentAdminEntryPoint::handles($request)) {
-    BookingPaymentAdminEntryPoint::run($root, $request);
-
-    exit;
-}
-
-$app->run();
+Application::boot($root)->run();
