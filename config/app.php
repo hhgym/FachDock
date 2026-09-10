ca9f0ea706f5c08f190e795e03560cf180c01641
Heinrich-Hertz-Gymnasium Berlin <50577548+hhgym-berlin@users.noreply.github.com>
2026-09-10 21:51:00 +0200

    chore: bump version to 1.0.0-rc.2

--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ $root = dirname(__DIR__);
 return [
     'app' => [
         'name' => 'FachDock',
-        'version' => '1.0.0-rc.1',
+        'version' => '1.0.0-rc.2',
         'environment' => 'production',
         'debug' => false,
         'installed' => false,
